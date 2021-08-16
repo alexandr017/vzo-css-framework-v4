9f84af2dc91c8e9ff17ad276d5af31d226c62636
Vladislav Klopota <vladislav997@yahoo.com>
2021-08-16 17:55:41 +0300

    fix bugs

--- modules/short-codes/9-insert_video/pc.js
+++ modules/short-codes/9-insert_video/pc.js
@@ -3,6 +3,6 @@ let videos = document.getElementsByClassName('insert-video-block');
 for (i = 0; i < videos.length; i++) {
     videos[i].addEventListener('click', function(){
         let link = this.querySelector('img').getAttribute('data-video');
-        this.innerHTML = '<div class="iframe-shadow " style="line-height: 0;" ><iframe class="border-radius" width="476" height="268" src="'+ link +'"></iframe></div>';
+        this.innerHTML = '<div class="iframe-shadow " style="line-height: 0;" ><iframe class="border-radius" width="476" height="268" src="'+ link +'" ></iframe></div>';
     });
 }
