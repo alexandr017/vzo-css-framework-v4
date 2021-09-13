ae6888876c10f60857e5a5130255104541d0a9b6
Vladislav Klopota <vladislav997@yahoo.com>
2021-09-13 18:02:59 +0300

    fix bugs

--- modules/4-sorting/pc.js
+++ modules/4-sorting/pc.js
@@ -1,5 +1,17 @@
-$$('.sort-item').forEach( () => {
-    this.onclick = () => {
-        $$('.sub-menu-sort-container')[0].style.display = 'none';
+let toggleSortBlock = $$('.sort');
+toggleSortBlock.forEach(function (value, index) {
+    let hideSortLink = value.querySelectorAll('.sort-item'),
+        showSortBlock = value.querySelector('.main-sort'),
+        sortBlock = value.querySelector('.sub-nav_one-column');
+    showSortBlock.addEventListener('mouseover', event => {
+        sortBlock.style.display = 'block';
+    });
+    for (let i = 0; i < hideSortLink.length; i++) {
+        if (hideSortLink[i].style.display !== 'none') {
+            hideSortLink[i].addEventListener('click', event => {
+                sortBlock.style.display = 'none';
+
+            });
+        }
     }
 });
---
+++ modules/8-we-compare/pc.js
@@ -0,0 +1,8 @@
+let videos = $$('.we-compare-insert-video-block');
+
+for (let i = 0; i < videos.length; i++) {
+    videos[i].addEventListener('click', function(){
+        let link = this.querySelector('.video-img').getAttribute('data-video');
+        this.innerHTML = '<div class="iframe-shadow " style="line-height: 0;" ><iframe class="border-radius" style="width: 100%; height: 295px"  src="'+ link +'" ></iframe></div>';
+    });
+}
