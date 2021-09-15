e68b41f3896947b34b36c5d94d59d8219df8db04
Vladislav Klopota <vladislav997@yahoo.com>
2021-09-15 14:50:37 +0300

    fix bugs

---
+++ modules/19-calc-loans/pc.js
@@ -0,0 +1,24 @@
+function loanCalc() {
+    let loanNumInput = document.querySelectorAll('.overpayment-num-input');
+    let loanRangeInput = document.querySelectorAll('.overpayment-range-input');
+    for (let i = 0; i < loanNumInput.length; i++) {
+        function loanCalcChange(e) {
+            for (let j = 0; j < loanRangeInput.length; j++) {
+                let target = e.target;
+                if (e.target.type !== 'range') {
+                    target = loanRangeInput[j];
+                }
+                let min = target.min;
+                let max = target.max;
+                let val = target.value;
+                target.style.backgroundSize = (val - min) * 100 / (max - min) + '% 100%';
+            }
+        }
+
+        loanRangeInput.forEach(input => {
+            input.addEventListener('input', loanCalcChange)
+        });
+        loanNumInput[i].addEventListener('input', loanCalcChange);
+    }
+}
+document.addEventListener('DOMContentLoaded', loanCalc);
