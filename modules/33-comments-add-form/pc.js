6b3d9182df4e430987db249f64c6a040bcaa8e60
alex <sasha17@live.ru>
2021-10-03 22:27:12 +0300

    module 33 (comments add form) creating

---
+++ modules/33-comments-add-form/pc.js
@@ -0,0 +1,12 @@
+$$('.form-1').forEach(form => {
+    form.querySelectorAll('input, textarea').forEach(tag => {
+        tag.addEventListener('focus', () => {
+            form.classList.add('form-1-active');
+        });
+    });
+    form.querySelectorAll('input, textarea').forEach(tag => {
+        tag.addEventListener('focusout', () => {
+            form.classList.remove('form-1-active');
+        });
+    });
+});
