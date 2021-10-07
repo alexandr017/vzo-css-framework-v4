e4d2ea6dc66be10080e56a0ac2ac294a91825a9c
Vladislav Klopota <vladislav997@yahoo.com>
2021-10-07 14:56:23 +0300

    fix bugs

--- modules/33-comments-add-form/pc.js
+++ modules/33-comments-add-form/pc.js
@@ -1,12 +1,16 @@
-$$('.form-1').forEach(form => {
-    form.querySelectorAll('input, textarea').forEach(tag => {
-        tag.addEventListener('focus', () => {
-            form.classList.add('form-1-active');
+function focusForm() {
+    $$('.form-1').forEach(form => {
+        form.querySelectorAll('input, textarea').forEach(tag => {
+            tag.addEventListener('focus', () => {
+                form.classList.add('form-1-active');
+            });
+        });
+        form.querySelectorAll('input, textarea').forEach(tag => {
+            tag.addEventListener('focusout', () => {
+                form.classList.remove('form-1-active');
+            });
         });
     });
-    form.querySelectorAll('input, textarea').forEach(tag => {
-        tag.addEventListener('focusout', () => {
-            form.classList.remove('form-1-active');
-        });
-    });
-});
+}
+
+document.addEventListener('DOMContentLoaded', focusForm);
