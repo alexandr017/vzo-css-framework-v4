fcad21fc0bc0d265a52f49c8222c3993ee125d86
alex <sasha17@live.ru>
2021-10-19 16:24:31 +0300

    modules 5 (card-mfo) creating

---
+++ modules/5-card-mfo/pc.js
@@ -0,0 +1,17 @@
+
+let cardHiddenBlock = $$('.card-body');
+cardHiddenBlock.forEach(function (value, index) {
+    let cardButtonShow = value.querySelector('.card-more'),
+        cardButtonHide = value.querySelector('.card-less'),
+        hiddenCardInfoBlock = value.querySelector('.hidden-card-info');
+    cardButtonShow.addEventListener('click', event => {
+        cardButtonHide.style.display = 'flex';
+        cardButtonShow.style.display = 'none';
+        hiddenCardInfoBlock.style.display = 'block';
+    });
+    cardButtonHide.addEventListener('click', event => {
+        cardButtonShow.style.display = 'flex';
+        cardButtonHide.style.display = 'none';
+        hiddenCardInfoBlock.style.display = 'none';
+    });
+});
