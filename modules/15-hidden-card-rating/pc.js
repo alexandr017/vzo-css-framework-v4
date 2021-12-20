c5a8a6b20716cb17ac8792d7f604dfb7092e01cf
Vladislav Klopota <vladislav997@yahoo.com>
2021-12-20 17:49:36 +0200

    edit styles for 5-card-default, 10-offer-filters, 6-slider-trustpilot-review rewrite js for 15-hidden-card-rating

--- modules/15-hidden-card-rating/pc.js
+++ modules/15-hidden-card-rating/pc.js
@@ -1,18 +1,15 @@
-document.addEventListener("click", function(e) {
-    if(e.target && e.target.classList.contains("rating-more")) {
-        e.target.classList.toggle("active");
-        let panel = e.target.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-            e.target.innerHTML = 'Рейтинг <svg xmlns="http://www.w3.org/2000/svg" stroke-width="1.5" width="11" height="6" viewBox="0 0 11 6" fill="none">\n' +
-                '<path d="M10 1L5.5 5L1 1" stroke="#4FABFA"/>\n' +
-                '</svg>\n' +
-                '<span uk-icon="icon: chevron-down"></span> ';
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + 0+ "px";
-            e.target.innerHTML = 'Рейтинг <svg xmlns="http://www.w3.org/2000/svg" width="11" height="6" viewBox="0 0 11 6" fill="none">\n' +
-                '<path d="M1 5L5.5 1L10 5" stroke-width="1.5" stroke="#4FABFA"/>\n' +
-                '</svg> '+'<span uk-icon="icon: chevron-up"></span>';
+let hiddenCardBtn = document.querySelectorAll('.rating-more')[0];
+
+hiddenCardBtn.addEventListener('click', function(){
+    hiddenCardBtn.classList.toggle('active');
+
+})
+
+document.addEventListener('click',function (e) {
+    if(!e.target.classList.contains('active')){
+        var openedRating = document.getElementsByClassName('active');
+        if(openedRating.length != 0) {
+            openedRating[0].classList.remove('active');
         }
     }
-});
+},false)
