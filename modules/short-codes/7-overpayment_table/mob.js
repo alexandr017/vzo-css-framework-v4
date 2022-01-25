61f85936a315a89482a7a37ecec2a687f1669b2d
Vladislav Klopota <vladislav997@yahoo.com>
2022-01-25 17:59:59 +0200

    add changes for modules which used on company page(review)

---
+++ modules/short-codes/7-overpayment_table/mob.js
@@ -0,0 +1,33 @@
+let addTableWrap = $$(".overpayment-block")[0];
+let items = Array.from(addTableWrap.querySelectorAll("tr"));
+let loadTableMore = $$(".loadTableMoreRow")[0];
+let loadTableLess = $$(".hideTableLessRow")[0];
+hiddenTableStyle = "hiddenTableStyle";
+items.forEach(function (item, index) {
+    if (index > 7) {
+        item.classList.add(hiddenTableStyle);
+        loadTableLess.style.display = "none";
+    }
+});
+loadTableMore.addEventListener("click", function () {
+    [].forEach.call(document.querySelectorAll("." + hiddenTableStyle), function (
+        item
+    ) {
+        item.classList.remove(hiddenTableStyle);
+        if (document.querySelectorAll("." + hiddenTableStyle).length === 0) {
+            loadTableMore.style.display = "none";
+            loadTableLess.style.display = "flex";
+        }
+    });
+});
+loadTableLess.addEventListener("click", function () {
+    items.forEach(function (item, index) {
+        if (index > 7) {
+            item.classList.add(hiddenTableStyle);
+        }
+    });
+    if (document.querySelectorAll("." + hiddenTableStyle).length !== 7) {
+        loadTableMore.style.display = "flex";
+        loadTableLess.style.display = "none";
+    }
+});
--- modules/short-codes/9-insert_video/pc.js
+++ modules/short-codes/9-insert_video/pc.js
@@ -1,6 +1,6 @@
 let videoBlock = $$('.insert-video-block');
 
-for (let i = 0; i < videos.length; i++) {
+for (let i = 0; i < videoBlock.length; i++) {
     videoBlock [i].addEventListener('click', function(){
         let link = this.querySelector('.video-img').getAttribute('data-video');
         this.innerHTML = '<div class="iframe-shadow " style="line-height: 0;" ><iframe class="border-radius" style="width: 100%; height: 300px"  src="'+ link +'" ></iframe></div>';
