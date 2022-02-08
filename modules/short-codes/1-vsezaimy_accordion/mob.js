61ec17114c9d25e8141a39df92ed7a3b40a840e1
Vladislav Klopota <vladislav997@yahoo.com>
2022-02-08 17:55:49 +0200

    add changes for amp modules which used on main page

---
+++ modules/short-codes/1-vsezaimy_accordion/mob.js
@@ -0,0 +1,15 @@
+let acc = $$(".accordion");
+for (let i = 0; i < $$(".accordion").length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight){
+            panel.style.maxHeight = null;
+            panel.style.borderBottom = null;
+
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+            this.style.borderBottom = "0";
+        }
+    });
+}
