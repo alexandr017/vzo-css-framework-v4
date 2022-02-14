a319f6e1b91f7e32aa5ad53a37ed799932fe77eb
Vladislav Klopota <vladislav997@yahoo.com>
2022-02-14 17:43:10 +0200

    add changes for amp modules which used on main page

--- modules/73-qa-reply-body/pc.js
+++ modules/73-qa-reply-body/pc.js
@@ -7,7 +7,11 @@ let mainCommentFormBlock = $$('.comments-add-form')[0];
 for(let i=0;i<commentBtnBlock.length;i++) {
     commentBtnBlock[i].addEventListener('click',function (e) {
         let to_remove = this.parentNode.nextElementSibling;
-        commentBtnBlock[i].nextElementSibling.style.display = 'block';
+        if(commentBtnBlock[i].nextElementSibling != null) {
+            commentBtnBlock[i].nextElementSibling.style.display = 'block';
+        }else{
+            commentBtnBlock[i].style.display = 'block';
+        }
         if (to_remove == null) {
             commentMovingForm.style.display = 'block';
             commentBtnBlock[i].closest('.reply-bottom-block').insertAdjacentElement('afterend', commentMovingForm);
