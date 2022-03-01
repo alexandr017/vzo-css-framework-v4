f5505e6eea9692d8ae840a0e9dffc8a50a9cc973
alexandr017 <sasha17@live.ru>
2022-03-01 15:37:55 +0300

    module 7 fixed, short-code 9 fixed

--- modules/short-codes/9-insert_video/mob.js
+++ modules/short-codes/9-insert_video/mob.js
@@ -3,6 +3,6 @@ let videos = $$('.insert-video-block');
 for (let i = 0; i < videos.length; i++) {
     videos[i].addEventListener('click', function(){
         let link = this.querySelector('.video-img').getAttribute('data-video');
-        this.innerHTML = '<div class="iframe-shadow " style="line-height: 0;" ><iframe class="border-radius" style="width: 100%; height: 175px"  src="'+ link +'" ></iframe></div>';
+        this.innerHTML = '<div class="iframe-shadow " style="line-height: 0;" ><iframe allowfullscreen class="border-radius" style="width: 100%; height: 175px"  src="'+ link +'" ></iframe></div>';
     });
 }
