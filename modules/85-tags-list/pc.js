bd20c0712376d6113210d40ad6c6e12e19e2eb21
HasmikManucharyan <h_manucharyan@mail.ru>
2022-03-07 17:57:53 +0400

    changes for 6-slider-home,85-tags-list

--- modules/85-tags-list/pc.js
+++ modules/85-tags-list/pc.js
@@ -1,8 +1,43 @@
-$$('.more-tags-btn')[0].addEventListener('click',function () {
-   let list = this.nextElementSibling;
-   list.classList.toggle('showed-tags-list');
-   this.querySelector('svg').classList.toggle('rotate');
-   let text = this.querySelector('span').innerHTML;
-   text = (text === 'Больше') ? 'Меньше' : 'Больше';
-   this.querySelector('span').innerHTML = text;
+// $$('.more-tags-btn')[0].addEventListener('click',function () {
+//     let list = this.nextElementSibling;
+//     list.classList.toggle('showed-tags-list');
+//     this.querySelector('svg').classList.toggle('rotate');
+//     let text = this.querySelector('span').innerHTML;
+//     text = (text === 'Больше') ? 'Меньше' : 'Больше';
+//     this.querySelector('span').innerHTML = text;
+// });
+
+document.addEventListener('DOMContentLoaded', function(){
+    if(document.getElementsByClassName('tags-list-wrap-slider').length>0){
+        slideShow({
+            element:'.tags-list-wrap-slider',
+            slidesToShow:5,
+            slidesToScroll:1,
+            circleScroll:true,
+            height:'30',
+            responsive: [
+                {
+                    breakpoint: 1200,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 920,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 1,
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        })
+    }
 });
